08a39fd0d415398226f3a84cceee1ec4cd773bbe
pinisetti1236 <113924488+pinisetti1236@users.noreply.github.com>
2023-05-01 11:26:17 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,23 @@
+pipeline {
+     agent any
+     
+     stages {
+          stage('Build') {
+              steps {
+                  echo 'building..'
+                  sh 'docker --version'
+                  sh """
+                  docker images
+                  """
+             }
+          }
+          stage('Run') {
+              steps {
+                  echo 'Run the java code.. with a GitHub webhook'
+                  sh 'java Hello'
+                  
+                  }
+                }
+              
+          }
+       }
