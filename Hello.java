3fab9985b3c1c39267fc8d2adf2ef193dd8e3f8f
pinisetti1236 <113924488+pinisetti1236@users.noreply.github.com>
2023-05-01 12:13:28 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,23 +1,23 @@
 pipeline {
-     agent any
+    agent any
      
-     stages {
-          stage('Build') {
-              steps {
-                  echo 'building..'
-                  sh 'docker --version'
-                  sh """
-                  docker images
-                  """
+    stages {
+        stage('Build') {
+            steps {
+                echo 'building..'
+                sh 'docker --version'
+                sh """
+                docker images
+                """
              }
-          }
-          stage('Run') {
-              steps {
-                  echo 'Run the java code.. with a GitHub webhook'
-                  sh 'java Hello'
+         }
+         stage('Run') {
+             steps {
+                 echo 'Run the java code.. with a GitHub webhook'
+                 sh 'java Hello'
                   
-                  }
-                }
+             }
+         }
               
-          }
-       }
+     }
+ }
